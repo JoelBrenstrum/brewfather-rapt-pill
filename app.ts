804cc62c319f0a4d9778ab234e2099d530b1e00b
Joel (the one and only) Brenstrum <joel@nztradegroup.co.nz>
2022-04-05 20:47:16 +1200

    Niceties

--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
 import { Issuer } from 'openid-client';
 import 'dotenv/config'
 import fetch from 'node-fetch';
+import * as dayjs from 'dayjs';
 import { BrewfatherCustomSteamPayload, GetHydrometersResult } from './types';
 
 const identityUrl = "https://id.rapt.io/.well-known/openid-configuration";
@@ -27,10 +28,19 @@ async function getAccessToken() {
     const accessToken = grantResponse.access_token
     return accessToken
 }
+let timeout;
 let intervalPointer;
+const intervalMinutes = 15
+const intervalMs = intervalMinutes * 60 * 1000; //15 minutes
+const dateFormat = "DD-MMM-YYYY hh:mm:ss a";
+const round = (value: number, decimalPlaces = 2): number  => {
+    const decimals = 10 ** decimalPlaces;
+    return +(Math.round(+(value * decimals).toFixed(2)) / decimals).toFixed(decimalPlaces);
+}
 
 const dataPost = async () => {
     try {
+        clearTimeout(timeout);
 
         //authenticate
         const bearerToken = await getAccessToken();
@@ -39,38 +49,46 @@ const dataPost = async () => {
         //get all hydrometers
         const req = await fetch('https://api.rapt.io/api/Hydrometers/GetHydrometers', { headers: { authorization: `Bearer ${bearerToken}` } })
         console.log(req.status)
-        if(req.status != 200){
+        if (req.status != 200) {
             console.log(await req.text())
             return;
         }
         const res = await req.json() as GetHydrometersResult;
-
+        console.log(`Posting result to brewfather @ ${dayjs().format(dateFormat)}`)
         //post all hydrometers to brewfather
-        await Promise.allSettled(res.map((hydrometer) => {
+        await Promise.allSettled(res.map(async (hydrometer) => {
             const payload: BrewfatherCustomSteamPayload = {
-                name: `${hydrometer.name} - ${hydrometer.id}`,
-                temp: hydrometer.temperature,
-                gravity: (hydrometer.gravity ?? 0) / 1000,
+                name: `${hydrometer.name}`,
+                temp: round(hydrometer.temperature),
+                gravity: (round(hydrometer.gravity ?? 0, 0)) / 1000,
                 battery: hydrometer.battery,
-                comment: `Signal Strength: ${hydrometer.rssi}dB`
-            };
-            console.log('payload',payload );
-            fetch(brewfatherUrl, {
-                body: JSON.stringify(payload),
+                comment: `Signal Strength: ${hydrometer.rssi}dB; Battery: ${hydrometer.battery}%; Last Reading: ${dayjs(hydrometer.lastActivityTime).format(dateFormat)}`
+            } 
+            const body = new URLSearchParams(payload as any)
+            console.log('payload', payload);
+            const req = await fetch(brewfatherUrl, {
+                body,
                 method: 'POST',
             })
+            const res = await req.json()
+            console.log('brewfather post', res);
+            if (res.result == "ignored") {
+                timeout = setTimeout(dataPost, 60000);
+            }
         }));
-
-
 
     } catch (ex) {
         console.error(ex)
         clearInterval(intervalPointer);
         throw ex;
     }
+    if(timeout){
+        console.log(`Failed to send, sending again in 1 minute...`)
+    } else {
+        console.log(`Waiting to send ${intervalMinutes} minutes to send again...`)
+    }
 }
 
-const intervalTime = 15 * 60 * 1000; //15 minutes
 (async () => {
     console.log('###########')
     console.log('clientSecret', clientSecret)
@@ -79,7 +97,7 @@ const intervalTime = 15 * 60 * 1000; //15 minutes
     console.log('###########')
 
     dataPost();
-    intervalPointer = setInterval(dataPost, intervalTime);
+    intervalPointer = setInterval(dataPost, intervalMs);
 })();
 
 process.on("exit", () => {
